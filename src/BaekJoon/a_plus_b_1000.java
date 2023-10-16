3887a26448506b11934d8fd9be144157a9ede0ff
Hwan <76806770+sang-hwann@users.noreply.github.com>
2023-10-16 17:56:49 +0900

    더하기, 출력 문제 풀이 - 밑 푸쉬테스트

---
+++ src/BaekJoon/a_plus_b_1000.java
@@ -0,0 +1,13 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class a_plus_b_1000 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        System.out.println(x+y);
+
+    }
+}
