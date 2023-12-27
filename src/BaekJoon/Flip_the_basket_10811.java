25c9dcb2323e768b76810806bcb37da6206b72c3
skyi0909@naver.com <skyi0909@naver.com>
2023-12-27 23:37:59 +0900

    배열 , 바구니 뒤집기 문제 코드

--- src/BaekJoon/Flip_the_basket_10811.java
+++ src/BaekJoon/Flip_the_basket_10811.java
@@ -1,10 +1,52 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class Flip_the_basket_10811 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); //입력을 받은 scanner 선언
+        int basketNum = scanner.nextInt(); // 바구니의 갯수 입력
+        int[] baskets = new int[basketNum]; //값을 받을 배열값 구성
+        int flipNum = scanner.nextInt(); //뒤집을 값 입력하는 횟수
+
+
+        int flipStartNum = 0; //뒤집기 시작 값
+        int flipEndNum = 0; //뒤집기 끝 값
+
+        //바구니 , 배열에 값 기본적으로 입력
+        for (int i = 0; i < basketNum; i++) {
+            baskets[i] = i + 1; // 배열에 값 입력
+        }
+
+        int differenceNum = 0;
+        int tempNum = 0;
+
+        //뒤집을 바구니들 입력
+        for (int i = 0; i < flipNum; i++) {
+            flipStartNum = scanner.nextInt();
+            flipEndNum = scanner.nextInt();
+            flipStartNum = flipStartNum - 1;
+            flipEndNum = flipEndNum - 1;
+
+            //뒤집기 , 뒤집을때 횟수가 절반만 뒤집음 ,
+            for (int j = flipStartNum; j <= (flipEndNum+flipStartNum)/2; j++) {
+                differenceNum = j - flipStartNum;
+                tempNum = baskets[j]; //시작 값 임시 저장
+                baskets[j] = baskets[flipEndNum - differenceNum]; //값 뒤집기
+                baskets[flipEndNum - differenceNum] = tempNum;
+            }
+        }
+
+        for (int i = 0; i < baskets.length; i++) {
+            System.out.print(baskets[i] + " ");
+        }
 
     }
 }
+
+
+
+
 //    바구니 뒤집기
 //문제
 //        도현이는 바구니를 총 N개 가지고 있고, 각각의 바구니에는 1번부터 N번까지 번호가 순서대로 적혀져 있다. 바구니는 일렬로 놓여져 있고, 가장 왼쪽 바구니를 1번째 바구니, 그 다음 바구니를 2번째 바구니, ..., 가장 오른쪽 바구니를 N번째 바구니라고 부른다.
